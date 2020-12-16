b63ea4382c26d4534065604c34d2788afcb59fa6
hongkhanhsd99 <55022059+hongkhanhsd99@users.noreply.github.com>
2020-12-16 21:10:26 +0700

    fix

---
+++ e-learning/footer.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+</head>
+<body>
+	<footer id="footer">
+		<div class="container">
+			<div class="social text-center">
+				<a href="#"><i class="fa fa-instagram"></i></a>
+				<a href="https://www.facebook.com/le.hongkhanh.1999"><i class="fa fa-facebook"></i></a>
+				<a href="#"><i class="fa fa-flickr"></i></a>
+				<a href="#"><i class="fa fa-github"></i></a>
+			</div>
+
+			<div class="clear"></div>
+			<!--CLEAR FLOATS-->
+		</div>
+		<div class="footer2">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-6 panel">
+						<div class="panel-body">
+							<p class="simplenav">
+								<a href="index.php">Home</a> | 
+								<a href="login.php">Login</a> | 
+								<a href="login.php">Courses</a> |
+								<a href="feedback.php">Feedback</a>
+							</p>
+						</div>
+					</div>
+
+					
+
+				</div>
+				<!-- /row of panels -->
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
---
+++ e-learning/nav.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+</head>
+<body>
+	<!-- Fixed navbar -->
+	<div class="navbar navbar-inverse">
+		<div class="container">
+			<div class="navbar-header">
+				<!-- Button for smallest screens -->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+				<a class="navbar-brand" href="index.php">
+					<img src="assets/images/newlogo.png" alt="E-learning"></a>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav pull-right mainNav">
+					<li class="active"><a href="index.php">Home</a></li>
+					<li><a href="login.php">Login</a></li>
+					<li><a href="login.php">Courses</a></li>
+					<li><a href="feedback.php">Feedback</a></li>			
+				</ul>
+			</div>
+			<!--/.nav-collapse -->
+		</div>
+	</div>
+	<!-- /.navbar -->
+</body>
+</html>
